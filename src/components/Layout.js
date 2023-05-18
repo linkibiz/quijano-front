9a321807c286aed5b57d1a407810de7d7f16b831
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-05-18 14:28:19 -0500

    ajustes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,9 +8,9 @@ const Layout = ({pageName, children}) => {
         <title>{title}</title>
         <link rel='shortcut icon' href='/grupo-residencial-fav-icon.png'/>
       </Head>
-      <main className="h-full bg-slate-100 min-h-screen flex justify-center">
-        <div className="bg-[#eceded] w-[768px] flex flex-col min-h-screen h-full overflow-hidden">
-          {children}    
+      <main className="bg-[#BEBEBE] h-full min-h-screen flex justify-center">
+        <div className="bg-gradient-pattern w-[425px] flex flex-col min-h-screen h-full">
+          { children }
         </div>
       </main>
     </div>
