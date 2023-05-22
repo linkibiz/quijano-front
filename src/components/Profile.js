22c35a4f235203f1322d352f3da970929e321210
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-05-22 09:22:59 -0500

    ajustes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,7 +17,7 @@ const Profile = ({ profileData }) => {
   return (
     <>
       <div className="-space-x-1 mt-[-45%] relative z-10 shadow-lg rounded-3xl">
-        <div className="flex items-center bg-white rounded-3xl">
+        <div className="flex items-center bg-[#E4E6E5] rounded-3xl">
           <Image
             className="rounded-l-3xl inline-block object-cover h-52 w-52"
             loader={myLoader}
@@ -28,8 +28,8 @@ const Profile = ({ profileData }) => {
           />
           <div className="text-black w-full px-4 flex flex-col gap-y-3.5 items-center">
             <Image src={Logo} height={100} width={100} />
-            <h1 className="text-[#84909C] font-bold text-4xl text-center">{nombre_apellido}</h1>
-            <p className="text-[#84909C] text-xs tracking-[3px] font-bold">{description}</p>
+            <h1 className="text-[#3F414B] font-bold text-4xl text-center">{nombre_apellido}</h1>
+            <p className="text-[#3F414B] text-xs tracking-[3px] font-bold">{description}</p>
           </div>
         </div>
       </div>
